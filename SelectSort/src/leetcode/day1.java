1909bb10df85ac09f1992a14e3169932e836ab37
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-09 22:43:49 +0800

    Create day1.java

---
+++ SelectSort/src/leetcode/day1.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode
+ * @createTime : 2022/8/9 22:06
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class day1 {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + ((right - left) >>> 1);
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[right] == target ? right : -1;
+    }
+
+}

04e7420000a8b880c88416f41285433568f80b25
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-17 16:41:53 +0800

    Update day2.java

--- SelectSort/src/leetcode/day2.java
+++ SelectSort/src/leetcode/day2.java
@@ -96,9 +96,37 @@ public class day2 {
         return ret;
     }
 
+
     public int orangesRotting(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        
+        int time = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    orangesRotting_dfs(grid, i, j, 2);
+                }
+            }
+        }
+        for (int[] ints : grid) {
+            for (int j = 0; j < n; j++) {
+                if (ints[j] == 1) {
+                    return -1;
+                }
+                time = Math.max(time, ints[j]);
+            }
+        }
+        return time == 0 ? 0 : time - 2;
+    }
+
+    private void orangesRotting_dfs(int[][] grid, int i, int j, int time) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || (grid[i][j] != 1 && grid[i][j] < time)) {
+            return;
+        }
+        grid[i][j] = time;
+        orangesRotting_dfs(grid, i - 1, j, time + 1);
+        orangesRotting_dfs(grid, i + 1, j, time + 1);
+        orangesRotting_dfs(grid, i, j - 1, time + 1);
+        orangesRotting_dfs(grid, i, j + 1, time + 1);
     }
 
 

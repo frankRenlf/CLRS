fd1afaf055202a98c634425f6f3421a3204561ed
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-18 12:59:11 +0800

    Update day10.java

--- SelectSort/src/leetcode/day10.java
+++ SelectSort/src/leetcode/day10.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import leetcode.algorithm.ListNode;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,4 +15,33 @@ package leetcode;
  * @Description :
  */
 public class day10 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode pre = new ListNode(Integer.MIN_VALUE);
+        ListNode cur = pre;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                cur.next = list2;
+                list2 = list2.next;
+            } else {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = list1 == null ? list2 : list1;
+        return pre.next;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
 }

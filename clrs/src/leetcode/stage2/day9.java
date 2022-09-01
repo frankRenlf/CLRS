40f5d51fd1691b84f730f286448004036e0d7e29
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-01 15:43:04 +0800

    Update day9.java

--- clrs/src/leetcode/stage2/day9.java
+++ clrs/src/leetcode/stage2/day9.java
@@ -1,5 +1,9 @@
 package leetcode.stage2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -29,5 +33,33 @@ public class day9 {
         }
         return ret;
     }
+
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> perm = new ArrayList<Integer>();
+        boolean[] visited = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(visited, nums, ans, 0, perm);
+        return ans;
+    }
+
+    public void backtrack(boolean[] visited, int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (visited[i] || (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            visited[i] = true;
+            backtrack(visited, nums, ans, idx + 1, perm);
+            visited[i] = false;
+            perm.remove(idx);
+        }
+    }
+
 
 }

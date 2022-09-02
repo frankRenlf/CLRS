ffa3ac1cbceacf450cb3b79f532f3d0571aa42e0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-02 11:20:44 +0800

    Create day10.java

---
+++ clrs/src/leetcode/stage2/day10.java
@@ -0,0 +1,43 @@
+package leetcode.stage2;
+
+import leetcode.algorithm.TreeNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.stage2
+ * @createTime : 2022/9/2 10:55
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class day10 {
+
+    int res;
+
+    public int longestUnivaluePath(TreeNode root) {
+        res = 0;
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left), right = dfs(root.right);
+        int left1 = 0, right1 = 0;
+        if (root.left != null && root.left.val == root.val) {
+            left1 = left + 1;
+        }
+        if (root.right != null && root.right.val == root.val) {
+            right1 = right + 1;
+        }
+        res = Math.max(res, left1 + right1);
+        return Math.max(left1, right1);
+    }
+
+}

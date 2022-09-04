1899cd39a5505d42f4f4dcb1ad612d6c538c0a08
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-04 12:33:06 +0800

    add numsSpecial method

--- clrs/src/leetcode/stage2/day12.java
+++ clrs/src/leetcode/stage2/day12.java
@@ -15,7 +15,23 @@ package leetcode.stage2;
 public class day12 {
 
     public int numSpecial(int[][] mat) {
-        
+        int row = mat.length, col = mat[0].length;
+        int[] rowSum = new int[row], colSum = new int[col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                colSum[j] += mat[i][j];
+                rowSum[i] += mat[i][j];
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (mat[i][j] == 1 && colSum[j] == 1 && rowSum[i] == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
 }

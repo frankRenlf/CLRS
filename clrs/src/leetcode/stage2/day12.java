87572ee7e9c0ebd997f367a23c19a39dc03be014
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-04 12:38:17 +0800

    add uniquePath method

--- clrs/src/leetcode/stage2/day12.java
+++ clrs/src/leetcode/stage2/day12.java
@@ -1,5 +1,7 @@
 package leetcode.stage2;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -32,6 +34,22 @@ public class day12 {
             }
         }
         return count;
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
 }

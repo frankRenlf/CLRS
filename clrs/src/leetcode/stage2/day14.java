3bbf95f42c475693f3734f8f4a0b8f1e596127f4
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-06 13:36:13 +0800

    numDecodings

--- clrs/src/leetcode/stage2/day14.java
+++ clrs/src/leetcode/stage2/day14.java
@@ -63,6 +63,21 @@ public class day14 {
         return sum;
     }
 
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] += f[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[n];
+    }
+
     static day14 d14 = new day14();
 
     public static void main(String[] args) {

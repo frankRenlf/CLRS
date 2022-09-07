2b5080998e21677fe3eb61b294183108120bf062
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-07 09:49:26 +0800

    update

---
+++ clrs/src/leetcode/stage2/day15.java
@@ -0,0 +1,34 @@
+package leetcode.stage2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.stage2
+ * @createTime : 2022/9/7 9:27
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class day15 {
+
+    public String reorderSpaces(String text) {
+        int count = 0;
+        StringBuilder ret = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            count++;
+        }
+        int sub = count/
+        return ret.toString();
+    }
+
+    static day15 d15 = new day15();
+
+    public static void main(String[] args) {
+        System.out.println(d15.reorderSpaces("  this   is  a sentence "));
+    }
+
+
+}

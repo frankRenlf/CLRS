bc0070f0b8c5cb6bb6fadaa56d09717565fcbbe8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-08 09:51:53 +0800

    Update day16.java

--- clrs/src/leetcode/stage2/day16.java
+++ clrs/src/leetcode/stage2/day16.java
@@ -15,10 +15,21 @@ package leetcode.stage2;
 public class day16 {
 
     public int[] constructArray(int n, int k) {
-        int[] ret = new int[n];
-        for (int i = 0; i < n - k; i++) {
-
+        int[] answer = new int[n];
+        int idx = 0;
+        for (int i = 1; i < n - k; ++i) {
+            answer[idx] = i;
+            ++idx;
         }
+        for (int i = n - k, j = n; i <= j; ++i, --j) {
+            answer[idx] = i;
+            ++idx;
+            if (i != j) {
+                answer[idx] = j;
+                ++idx;
+            }
+        }
+        return answer;
     }
 
 }

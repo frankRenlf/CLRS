0026893685c5b52a10c141326846b4bea834de54
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-08 10:18:08 +0800

    Update day16.java

--- clrs/src/leetcode/stage2/day16.java
+++ clrs/src/leetcode/stage2/day16.java
@@ -13,6 +13,11 @@ package leetcode.stage2;
  * @Description :
  */
 public class day16 {
+    // n-1 k
+    // 1-k
+    // n-k-1
+
+    // [1,2,⋯,n−k,n,n−k+1,n−1,n−k+2,⋯]
 
     public int[] constructArray(int n, int k) {
         int[] answer = new int[n];
@@ -30,6 +35,36 @@ public class day16 {
             }
         }
         return answer;
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length(), len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return len1 + len2 - 2 * dp[len1][len2];
     }
 
 }

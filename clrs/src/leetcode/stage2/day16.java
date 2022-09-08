544812f07664a64c19e87f006eb49a8b6027ccaf
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-08 09:46:09 +0800

    Create day16.java

---
+++ clrs/src/leetcode/stage2/day16.java
@@ -0,0 +1,24 @@
+package leetcode.stage2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.stage2
+ * @createTime : 2022/9/8 9:44
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class day16 {
+
+    public int[] constructArray(int n, int k) {
+        int[] ret = new int[n];
+        for (int i = 0; i < n - k; i++) {
+
+        }
+    }
+
+}

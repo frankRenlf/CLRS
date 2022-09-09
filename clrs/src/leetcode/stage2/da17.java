fa89377d16739595e04680250030c42722a998e8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-09 09:54:01 +0800

    Create da17.java

---
+++ clrs/src/leetcode/stage2/da17.java
@@ -0,0 +1,55 @@
+package leetcode.stage2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.stage2
+ * @createTime : 2022/9/9 9:21
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class da17 {
+
+    public int minOperations(String[] logs) {
+        int pos = 0;
+        for (String log : logs) {
+            if (log.charAt(0) != '.') {
+                pos++;
+            } else if (log.charAt(1) == '.') {
+                pos = pos > 0 ? pos - 1 : 0;
+            }
+        }
+        return pos;
+    }
+
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(Math.max(2 * (i - 2), 2 * dp[i - 2]), Math.max(3 * (i - 3), 3 * dp[i - 3]));
+        }
+        return dp[n];
+    }
+
+    public int integerBreak2(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            int curMax = 0;
+            for (int j = 1; j < i; j++) {
+                curMax = Math.max(curMax, Math.max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax;
+        }
+        return dp[n];
+    }
+    
+
+
+}

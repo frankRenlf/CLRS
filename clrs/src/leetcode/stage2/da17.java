24ee2be0ba9ce9b05c4701b1421ef45fe47b77ee
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-09 09:54:15 +0800

    Update da17.java

--- clrs/src/leetcode/stage2/da17.java
+++ clrs/src/leetcode/stage2/da17.java
@@ -49,7 +49,21 @@ public class da17 {
         }
         return dp[n];
     }
+
+    public int integerBreak3(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int quotient = n / 3;
+        int remainder = n % 3;
+        if (remainder == 0) {
+            return (int) Math.pow(3, quotient);
+        } else if (remainder == 1) {
+            return (int) Math.pow(3, quotient - 1) * 4;
+        } else {
+            return (int) Math.pow(3, quotient) * 2;
+        }
+    }
+
     
-
-
 }

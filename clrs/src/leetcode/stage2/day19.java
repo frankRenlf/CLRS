c001ae979d881e64744a412ec846b3a44f026f0d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-11 18:19:40 +0800

    Update day19.java

--- clrs/src/leetcode/stage2/day19.java
+++ clrs/src/leetcode/stage2/day19.java
@@ -81,6 +81,51 @@ public class day19 {
         return ret;
     }
 
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int max = 1;
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] count = new int[n];
+        int max = 1;
+        Arrays.fill(dp, 1);
+        Arrays.fill(count, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == max) {
+                cnt += count[i];
+            }
+        }
+        return cnt;
+    }
+
     static day19 d19 = new day19();
 
     public static void main(String[] args) {

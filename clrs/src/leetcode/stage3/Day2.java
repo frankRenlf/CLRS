14a893a5dcd6de01003b01d8df67c803d2129f30
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-13 09:24:14 +0800

    update

--- clrs/src/leetcode/stage3/Day2.java
+++ clrs/src/leetcode/stage3/Day2.java
@@ -2,7 +2,6 @@ package leetcode.stage3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,12 +18,39 @@ import java.util.Stack;
 public class Day2 {
 
     public int maximumSwap(int num) {
-        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
         while (num != 0) {
-            stack.push(num % 10);
+            list.add(0, num % 10);
             num /= 10;
         }
-
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            int index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (list.get(j) >= list.get(index)) {
+                    index = j;
+                }
+            }
+            if (list.get(i) < list.get(index)) {
+                int tmp = list.get(i);
+                list.set(i, list.get(index));
+                list.set(index, tmp);
+                break;
+            }
+        }
+        int ret = 0;
+        for (int i = 0;i <n; i++) {
+            ret *= 10;
+            ret += list.get(i);
+        }
+        return ret;
     }
+
+    static Day2 day2 = new Day2();
+
+    public static void main(String[] args) {
+        System.out.println(day2.maximumSwap(2736));
+    }
+
 
 }

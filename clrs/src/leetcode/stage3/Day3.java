7376e5e3e2bc9e47ebb0b37882573508d880032a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-15 12:07:39 +0800

    update

--- clrs/src/leetcode/stage3/Day3.java
+++ clrs/src/leetcode/stage3/Day3.java
@@ -1,6 +1,8 @@
 package leetcode.stage3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,6 +27,37 @@ public class Day3 {
             sum += arr[i];
         }
         return sum / (n - sub * 2);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> ret = new ArrayList<>();
+        if (nums == null || n == 0) return ret;
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    while (left < right && nums[right] == nums[--right]) ;
+                } else if (sum == 0) {
+                    ret.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    while (left < right && nums[left] == nums[++left]) ;
+                    while (left < right && nums[right] == nums[--right]) ;
+                } else {
+                    while (left < right && nums[left] == nums[++left]) ;
+                }
+            }
+        }
+        return ret;
+    }
+
+    static Day3 day3 = new Day3();
+
+    public static void main(String[] args) {
+        System.out.println(day3.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
 }
---
+++ clrs/src/leetcode/stage3/Day4.java
@@ -0,0 +1,45 @@
+package leetcode.stage3;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.stage3
+ * @createTime : 2022/9/15 12:06
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Day4 {
+    public int flipLights(int n, int presses) {
+        Set<Integer> seen = new HashSet<Integer>();
+        for (int i = 0; i < 1 << 4; i++) {
+            int[] pressArr = new int[4];
+            for (int j = 0; j < 4; j++) {
+                pressArr[j] = (i >> j) & 1;
+            }
+            int sum = Arrays.stream(pressArr).sum();
+            if (sum % 2 == presses % 2 && sum <= presses) {
+                int status = pressArr[0] ^ pressArr[1] ^ pressArr[3];
+                if (n >= 2) {
+                    status |= (pressArr[0] ^ pressArr[1]) << 1;
+                }
+                if (n >= 3) {
+                    status |= (pressArr[0] ^ pressArr[2]) << 2;
+                }
+                if (n >= 4) {
+                    status |= status << 3;
+                }
+                seen.add(status);
+            }
+        }
+        return seen.size();
+    }
+
+}

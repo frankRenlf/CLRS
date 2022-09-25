d283f1ed5f2cc5c0d63eed94e51ac1bbd8ef9280
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-25 20:13:04 +0800

    Update Day16.java

--- clrs/src/leetcode/stage3/Day16.java
+++ clrs/src/leetcode/stage3/Day16.java
@@ -20,6 +20,30 @@ import java.util.List;
  */
 public class Day16 {
 
+    public int sumNumbers(TreeNode root) {
+        
+    }
+
+
+    List<TreeNode> list = new ArrayList<>();
+
+    public void flatten(TreeNode root) {
+        if(root==null)return;
+        addRoot(root);
+        TreeNode cur = root;
+        for (int i = 1; i < list.size(); i++) {
+            cur.left = null;
+            cur.right = list.get(i);
+            cur = cur.right;
+        }
+    }
+
+    private void addRoot(TreeNode root) {
+        if (root == null) return;
+        list.add(root);
+        addRoot(root.left);
+        addRoot((root.right));
+    }
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> ret = new ArrayList<>();
         List<Integer> list = new ArrayList<>();

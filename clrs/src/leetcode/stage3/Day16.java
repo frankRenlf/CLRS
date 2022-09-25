47d85ea6d6c6dbd3d76a16930b52670512a71ccf
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-25 15:05:54 +0800

    update

--- clrs/src/leetcode/stage3/Day16.java
+++ clrs/src/leetcode/stage3/Day16.java
@@ -1,5 +1,7 @@
 package leetcode.stage3;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +16,37 @@ package leetcode.stage3;
  */
 public class Day16 {
 
-    
+    static int[] DIFFS = {0, 0, 1, -1, -1, 1, 1, -1, 0, 1};
+
+    char[] s;
+    int[][] dp;
+
+    public int rotatedDigits(int n) {
+        s = Integer.toString(n).toCharArray();
+        int m = s.length;
+        dp = new int[m][2];
+        for (int i = 0; i < m; i++) Arrays.fill(dp[i], -1);
+        return f(0, 0, true);
+    }
+
+    int f(int i, int hasDiff, boolean isLimit) {
+        if (i == s.length) {
+            System.out.println(hasDiff);
+            return hasDiff; // 只有包含 2/5/6/9 才算一个好数
+        }
+        if (!isLimit && dp[i][hasDiff] >= 0) return dp[i][hasDiff];
+        int res = 0;
+        int up = isLimit ? s[i] - '0' : 9;
+        for (int d = 0; d <= up; ++d) // 枚举要填入的数字 d
+            if (DIFFS[d] != -1) // d 不是 3/4/7
+                res += f(i + 1, hasDiff | DIFFS[d], isLimit && d == up);
+        if (!isLimit) dp[i][hasDiff] = res;
+        return res;
+    }
+    static Day16 day16=new Day16();
+
+    public static void main(String[] args) {
+        day16.rotatedDigits(12);
+    }
 
 }

eb951e6efcb1f82c10db6c1945f042dc4de60013
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-05 09:33:04 +0800

    Create Day24.java

---
+++ clrs/src/leetcode/stage3/Day24.java
@@ -0,0 +1,37 @@
+package leetcode.stage3;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.stage3
+ * @createTime : 2022/10/5 9:30
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Day24 {
+
+    public int minAddToMakeValid(String s) {
+        int ans = 0;
+        int leftCount = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                leftCount++;
+            } else {
+                if (leftCount > 0) {
+                    leftCount--;
+                } else {
+                    ans++;
+                }
+            }
+        }
+        ans += leftCount;
+        return ans;
+    }
+
+}

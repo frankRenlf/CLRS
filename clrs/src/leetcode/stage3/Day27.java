5df02ad71fe4f5f5596b531a8a6e4b4731afbeb0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-08 10:57:52 +0800

    Create Day27.java

---
+++ clrs/src/leetcode/stage3/Day27.java
@@ -0,0 +1,49 @@
+package leetcode.stage3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.stage3
+ * @createTime : 2022/10/8 10:46
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Day27 {
+
+    public int[] advantageCount(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        Integer[] index1 = new Integer[n], index2 = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            index1[i] = index2[i] = i;
+        }
+        Arrays.sort(index1, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return nums1[o1] - nums1[o2];
+            }
+        });
+        Arrays.sort(index2, (a, b) -> {
+            return nums2[a] - nums2[b];
+        });
+        int left = 0, right = n - 1;
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (nums1[index1[i]] > nums2[index2[left]]) {
+                ret[index2[left]] = nums1[index1[i]];
+                left++;
+            } else {
+                ret[index2[right]] = nums1[index1[i]];
+                right--;
+            }
+        }
+        return ret;
+    }
+
+}

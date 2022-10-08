dc7c290dad01569b4431700230c2ca4f201d50c2
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-08 11:01:35 +0800

    modify the sort algorithm

--- clrs/src/leetcode/stage3/Day27.java
+++ clrs/src/leetcode/stage3/Day27.java
@@ -19,27 +19,22 @@ public class Day27 {
 
     public int[] advantageCount(int[] nums1, int[] nums2) {
         int n = nums1.length;
-        Integer[] index1 = new Integer[n], index2 = new Integer[n];
+        Integer[] index2 = new Integer[n];
         for (int i = 0; i < n; i++) {
-            index1[i] = index2[i] = i;
+            index2[i] = i;
         }
-        Arrays.sort(index1, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return nums1[o1] - nums1[o2];
-            }
-        });
+        Arrays.sort(nums1);
         Arrays.sort(index2, (a, b) -> {
             return nums2[a] - nums2[b];
         });
         int left = 0, right = n - 1;
         int[] ret = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (nums1[index1[i]] > nums2[index2[left]]) {
-                ret[index2[left]] = nums1[index1[i]];
+        for (int j : nums1) {
+            if (j > nums2[index2[left]]) {
+                ret[index2[left]] = j;
                 left++;
             } else {
-                ret[index2[right]] = nums1[index1[i]];
+                ret[index2[right]] = j;
                 right--;
             }
         }

7d102dc758663a603a96535a9b624fa5ccaa8a16
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-11 10:49:03 +0800

    Create T1790.java

---
+++ clrs/src/leetcode/dailyWork/T1790.java
@@ -0,0 +1,21 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/10/11 10:48
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1790 {
+
+    public boolean areAlmostEqual(String s1, String s2) {
+        
+    }
+
+}

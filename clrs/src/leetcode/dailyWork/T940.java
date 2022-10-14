c13e1d5babb7b7d8b4d132436187c4e541801096
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-14 13:18:42 +0800

    update

---
+++ clrs/src/leetcode/dailyWork/T940.java
@@ -0,0 +1,48 @@
+package leetcode.dailyWork;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/10/14 13:05
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T940 {
+
+    int sum = 0;
+
+    public int distinctSubseqII(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        findSub(arr, n, 0);
+        return sum;
+    }
+
+    private void findSub(char[] arr, int n, int index) {
+        if (index == n) return;
+        Set<Character> set = new HashSet<>();
+        for (int i = index; i < n; i++) {
+            if (i > index && set.contains(arr[i])) {
+                continue;
+            }
+            set.add(arr[i]);
+            sum++;
+            findSub(arr, n, i + 1);
+        }
+    }
+
+    static T940 t940 = new T940();
+
+    public static void main(String[] args) {
+        System.out.println(t940.distinctSubseqII("aba"));
+    }
+
+}

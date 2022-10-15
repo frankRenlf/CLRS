8cdbc6fd5fdc0977fddd2bbc8c92f257b8c90358
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-15 10:36:46 +0800

    Create T1441.java

---
+++ clrs/src/leetcode/dailyWork/T1441.java
@@ -0,0 +1,35 @@
+package leetcode.dailyWork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/10/15 10:30
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1441 {
+
+    public List<String> buildArray(int[] target, int n) {
+        int length = target.length;
+        List<String> list = new ArrayList<>();
+        for (int i = 1, index = 0; i <= target[length - 1]; i++) {
+            if (target[index] == i) {
+                list.add("push");
+                index++;
+            } else {
+                list.add("push");
+                list.add("pop");
+            }
+        }
+        return list;
+    }
+
+}

f55d5076b944152cf6ed5f8481d563e0124bc67f
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-19 11:04:39 +0800

    initiate new daily work

---
+++ clrs/src/leetcode/dailyWork/T1700.java
@@ -0,0 +1,21 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/10/19 11:03
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1700 {
+
+    public int countStudents(int[] students, int[] sandwiches) {
+        
+    }
+
+}

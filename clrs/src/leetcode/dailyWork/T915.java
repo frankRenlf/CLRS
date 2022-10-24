dfc3909e9fc95fbc3b3c180fa2c37f0bf4712018
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-24 10:31:33 +0800

    update

--- clrs/src/leetcode/dailyWork/T915.java
+++ clrs/src/leetcode/dailyWork/T915.java
@@ -14,8 +14,30 @@ package leetcode.dailyWork;
  */
 public class T915 {
 
-    public int partitionDisjoint(int[] nums) {
-        
+
+    public int partitionDisjoint1(int[] nums) {
+        int n = nums.length;
+        int leftMax = nums[0], leftPos = 0, curMax = nums[0];
+        for (int i = 1; i < n - 1; i++) {
+            curMax = Math.max(curMax, nums[i]);
+            if (nums[i] < leftMax) {
+                leftMax = curMax;
+                leftPos = i;
+            }
+        }
+        return leftPos + 1;
     }
 
+    public int partitionDisjoint(int[] nums) {
+        int n = nums.length;
+        int lm = nums[0], lp = 0, cm = nums[0];
+        for (int i = 1; i < n - 1; i++) {
+            cm = Math.max(cm, nums[i]);
+            if (nums[i] < lm) {
+                lm = cm;
+                lp = i;
+            }
+        }
+        return lp + 1;
+    }
 }

8fa819d1cb3f048f96986e8228e404ed1149048c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-29 10:12:21 +0800

    Create T219.java

---
+++ clrs/src/leetcode/dailyWork/T219.java
@@ -0,0 +1,45 @@
+package leetcode.dailyWork;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/10/29 10:05
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T219 {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if (i > k) set.remove(nums[i - k - 1]);
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+
+
+}

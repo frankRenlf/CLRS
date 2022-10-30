511631b858869e8c7c11398255ab85f1fb805b91
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-30 14:08:31 +0800

    Create T784.java

---
+++ clrs/src/leetcode/dailyWork/T784.java
@@ -0,0 +1,39 @@
+package leetcode.dailyWork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/10/30 13:58
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T784 {
+
+    List<String> list = new ArrayList<>();
+
+    public List<String> letterCasePermutation(String s) {
+        find(s.toCharArray(), 0, s.length());
+        return list;
+    }
+
+    private void find(char[] arr, int i, int end) {
+        if (i == end) {
+            list.add(new String(arr));
+            return;
+        }
+        if (Character.isAlphabetic(arr[i])) {
+            find(arr, i + 1, end);
+            arr[i] ^= 32;
+            find(arr, i + 1, end);
+            arr[i] ^= 32;
+        }
+    }
+}

ce669b19298753df1ffa2c5e0e8cc72d69188ce3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-31 10:22:05 +0800

    Create T481.java

---
+++ clrs/src/leetcode/dailyWork/T481.java
@@ -0,0 +1,23 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/10/31 10:21
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T481 {
+
+    public int magicalString(int n) {
+
+        
+
+    }
+
+}

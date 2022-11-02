80f67c92d3b738f8d60b6ca6e4d0c28662ac56ab
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-02 14:17:31 +0800

    Create T1620.java

---
+++ clrs/src/leetcode/dailyWork/T1620.java
@@ -0,0 +1,51 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/11/2 14:17
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1620 {
+
+    public int[] bestCoordinate(int[][] towers, int radius) {
+        int xMax = Integer.MIN_VALUE, yMax = Integer.MIN_VALUE;
+        for (int[] tower : towers) {
+            int x = tower[0], y = tower[1];
+            xMax = Math.max(xMax, x);
+            yMax = Math.max(yMax, y);
+        }
+        int cx = 0, cy = 0;
+        int maxQuality = 0;
+        for (int x = 0; x <= xMax; x++) {
+            for (int y = 0; y <= yMax; y++) {
+                int[] coordinate = {x, y};
+                int quality = 0;
+                for (int[] tower : towers) {
+                    int squaredDistance = getSquaredDistance(coordinate, tower);
+                    if (squaredDistance <= radius * radius) {
+                        double distance = Math.sqrt(squaredDistance);
+                        quality += (int) Math.floor(tower[2] / (1 + distance));
+                    }
+                }
+                if (quality > maxQuality) {
+                    cx = x;
+                    cy = y;
+                    maxQuality = quality;
+                }
+            }
+        }
+        return new int[]{cx, cy};
+    }
+
+    public int getSquaredDistance(int[] coordinate, int[] tower) {
+        return (tower[0] - coordinate[0]) * (tower[0] - coordinate[0]) + (tower[1] - coordinate[1]) * (tower[1] - coordinate[1]);
+    }
+
+}

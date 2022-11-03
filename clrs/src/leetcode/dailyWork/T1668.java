db5130adb104922268a9b9b433379290d168276d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-03 15:45:50 +0800

    Update T1668.java

--- clrs/src/leetcode/dailyWork/T1668.java
+++ clrs/src/leetcode/dailyWork/T1668.java
@@ -15,7 +15,20 @@ package leetcode.dailyWork;
 public class T1668 {
 
     public int maxRepeating(String sequence, String word) {
-
+        int ret = 0;
+        int sl = sequence.length(), wl = word.length();
+        for (int i = 0; i < sl; i++) {
+            int j = 0;
+            while (word.charAt(j) == sequence.charAt(i + j)) {
+                while (j < wl && word.charAt(j) == sequence.charAt(i + j)) {
+                    j++;
+                }
+                ret += j == wl ? 1 : 0;
+                i += j;
+                j = 0;
+            }
+        }
+        return ret;
     }
 
 }

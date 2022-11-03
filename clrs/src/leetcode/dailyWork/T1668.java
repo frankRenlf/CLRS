c151a90a84238e140a4e78d8e294c2280b1c581a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-03 15:37:48 +0800

    Create T1668.java

---
+++ clrs/src/leetcode/dailyWork/T1668.java
@@ -0,0 +1,21 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/11/3 15:37
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1668 {
+
+    public int maxRepeating(String sequence, String word) {
+
+    }
+
+}

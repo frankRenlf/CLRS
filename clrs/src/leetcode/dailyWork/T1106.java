d6d38fe0c397ecbb644d2950ea8e56f38a8f259a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-05 15:57:29 +0800

    Update T1106.java

--- clrs/src/leetcode/dailyWork/T1106.java
+++ clrs/src/leetcode/dailyWork/T1106.java
@@ -1,5 +1,7 @@
 package leetcode.dailyWork;
 
+import java.util.Stack;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -15,7 +17,41 @@ package leetcode.dailyWork;
 public class T1106 {
 
     public boolean parseBoolExpr(String expression) {
-
+        Stack<Character> stack = new Stack<>();
+        int n = expression.length();
+        for (int i = 0; i < n; i++) {
+            char c = expression.charAt(i);
+            if (c == ',') {
+                continue;
+            } else if (c != ')') {
+                stack.push(c);
+            } else {
+                int t = 0, f = 0;
+                while (stack.peek() != '(') {
+                    char val = stack.pop();
+                    if (val == 't') {
+                        t++;
+                    } else {
+                        f++;
+                    }
+                }
+                stack.pop();
+                char op = stack.pop();
+                switch (op) {
+                    case '!':
+                        stack.push(f == 1 ? 't' : 'f');
+                        break;
+                    case '&':
+                        stack.push(f == 0 ? 't' : 'f');
+                        break;
+                    case '|':
+                        stack.push(t > 0 ? 't' : 'f');
+                        break;
+                    default:
+                }
+            }
+        }
+        return stack.pop() == 't';
     }
 
 }

c9dc9e9223278fd036d45e7129f12bcac7d95637
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-06 16:55:54 +0800

    Create T1678.java

---
+++ clrs/src/leetcode/dailyWork/T1678.java
@@ -0,0 +1,36 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/11/6 16:51
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1678 {
+
+    public String interpret(String command) {
+        StringBuilder stringBuffer = new StringBuilder();
+        for (int i = 0; i < command.length(); ) {
+            if (command.charAt(i) == '(') {
+                if (command.charAt(i + 1) == ')') {
+                    stringBuffer.append('o');
+                    i += 2;
+                } else {
+                    stringBuffer.append("al");
+                    i += 4;
+                }
+            } else {
+                stringBuffer.append('G');
+                i += 2;
+            }
+        }
+        return stringBuffer.toString();
+    }
+
+}

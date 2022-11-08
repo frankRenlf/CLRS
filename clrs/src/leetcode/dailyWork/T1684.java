e5580510db43a4f1c1855e52c125a578f4e4a975
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-08 12:05:52 +0800

    Update T1684.java

--- clrs/src/leetcode/dailyWork/T1684.java
+++ clrs/src/leetcode/dailyWork/T1684.java
@@ -15,7 +15,22 @@ package leetcode.dailyWork;
 public class T1684 {
 
     public int countConsistentStrings(String allowed, String[] words) {
-
+        char[] arr = new char[26];
+        for (char c : allowed.toCharArray()) {
+            arr[c - 'a'] = 1;
+        }
+        int ret = 0;
+        for (String s : words) {
+            int i = 0;
+            for (char c : s.toCharArray()) {
+                if (arr[c - 'a'] == 0) {
+                    break;
+                }
+                i++;
+            }
+            if (i == s.length()) ret++;
+        }
+        return ret;
     }
 
 }

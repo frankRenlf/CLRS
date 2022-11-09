f15942c4d38e8014f74c5a58e90c8f37dfc3e0b0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-09 11:05:05 +0800

    Create T764.java

---
+++ clrs/src/leetcode/dailyWork/T764.java
@@ -0,0 +1,80 @@
+package leetcode.dailyWork;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/11/9 11:04
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T764 {
+
+
+    public int orderOfLargestPlusSign(int n, int[][] mines) {
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], n);
+        }
+        Set<Integer> banned = new HashSet<Integer>();
+        for (int[] vec : mines) {
+            banned.add(vec[0] * n + vec[1]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            /* left */
+            for (int j = 0; j < n; j++) {
+                if (banned.contains(i * n + j)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                dp[i][j] = Math.min(dp[i][j], count);
+            }
+            count = 0;
+            /* right */
+            for (int j = n - 1; j >= 0; j--) {
+                if (banned.contains(i * n + j)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                dp[i][j] = Math.min(dp[i][j], count);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            /* up */
+            for (int j = 0; j < n; j++) {
+                if (banned.contains(j * n + i)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                dp[j][i] = Math.min(dp[j][i], count);
+            }
+            count = 0;
+            /* down */
+            for (int j = n - 1; j >= 0; j--) {
+                if (banned.contains(j * n + i)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                dp[j][i] = Math.min(dp[j][i], count);
+                ans = Math.max(ans, dp[j][i]);
+            }
+        }
+        return ans;
+    }
+
+}

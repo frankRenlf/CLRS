9c633f97b5a9c0bb0ac5762b09aff6fd6241e102
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-14 08:38:47 +0800

    Create T805.java

---
+++ clrs/src/leetcode/dailyWork/T805.java
@@ -0,0 +1,59 @@
+package leetcode.dailyWork;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/11/14 8:38
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T805 {
+
+    public boolean splitArraySameAverage(int[] nums) {
+        if (nums.length == 1) {
+            return false;
+        }
+        int n = nums.length, m = n / 2;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        boolean isPossible = false;
+        for (int i = 1; i <= m; i++) {
+            if (sum * i % n == 0) {
+                isPossible = true;
+                break;
+            }
+        }
+        if (!isPossible) {
+            return false;
+        }
+        Set<Integer>[] dp = new Set[m + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i] = new HashSet<Integer>();
+        }
+        dp[0].add(0);
+        for (int num : nums) {
+            for (int i = m; i >= 1; i--) {
+                for (int x : dp[i - 1]) {
+                    int curr = x + num;
+                    if (curr * n == sum * i) {
+                        return true;
+                    }
+                    dp[i].add(curr);
+                }
+            }
+        }
+        return false;
+    }
+
+
+}

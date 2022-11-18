82ffccafb7ab1577b53d64e46a93eff064e58a28
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-18 10:19:05 +0800

    Create T891.java

---
+++ clrs/src/leetcode/dailyWork/T891.java
@@ -0,0 +1,32 @@
+package leetcode.dailyWork;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/11/18 10:18
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T891 {
+
+    public int sumSubseqWidths(int[] nums) {
+        final int MOD = 1000000007;
+        Arrays.sort(nums);
+        long res = 0;
+        long x = nums[0], y = 2;
+        for (int j = 1; j < nums.length; j++) {
+            res = (res + nums[j] * (y - 1) - x) % MOD;
+            x = (x * 2 + nums[j]) % MOD;
+            y = y * 2 % MOD;
+        }
+        return (int) ((res + MOD) % MOD);
+    }
+
+}

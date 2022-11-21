594256d876505f454cfee6d351adab2bf9869bc7
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-21 10:34:26 +0800

    update

---
+++ clrs/src/leetcode/dailyWork/T808.java
@@ -0,0 +1,35 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/11/21 10:34
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T808 {
+
+    public double soupServings(int n) {
+        n = (int) Math.ceil((double) n / 25);
+        if (n >= 179) {
+            return 1.0;
+        }
+        double[][] dp = new double[n + 1][n + 1];
+        dp[0][0] = 0.5;
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = 1.0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[Math.max(0, i - 4)][j] + dp[Math.max(0, i - 3)][Math.max(0, j - 1)] + dp[Math.max(0, i - 2)][Math.max(0, j - 2)] + dp[Math.max(0, i - 1)][Math.max(0, j - 3)]) / 4.0;
+            }
+        }
+        return dp[n][n];
+    }
+
+}

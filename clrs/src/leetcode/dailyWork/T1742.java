4d7b0f7bbf5cedd989ed1652b2d413b794e961ec
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-23 10:15:39 +0800

    Update T1742.java

--- clrs/src/leetcode/dailyWork/T1742.java
+++ clrs/src/leetcode/dailyWork/T1742.java
@@ -1,5 +1,8 @@
 package leetcode.dailyWork;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -15,7 +18,19 @@ package leetcode.dailyWork;
 public class T1742 {
 
     public int countBalls(int lowLimit, int highLimit) {
-
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        int res = 0;
+        for (int i = lowLimit; i <= highLimit; i++) {
+            int box = 0, x = i;
+            while (x != 0) {
+                box += x % 10;
+                x /= 10;
+            }
+            count.put(box, count.getOrDefault(box, 0) + 1);
+            res = Math.max(res, count.get(box));
+        }
+        return res;
     }
+
 
 }

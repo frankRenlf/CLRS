01c913303ca4f21eb80ccd755a0420c3d338335d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-24 09:00:11 +0800

    complete

--- clrs/src/leetcode/dailyWork/T795.java
+++ clrs/src/leetcode/dailyWork/T795.java
@@ -15,8 +15,19 @@ package leetcode.dailyWork;
 public class T795 {
 
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        int n = nums.length, ans = 0, i0 = -1, i1 = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > right) i0 = i;
+            if (nums[i] >= left) i1 = i;
+            ans += i1 - i0;
+        }
+        return ans;
+    }
+
+
+    public int numSubarrayBoundedMax1(int[] nums, int left, int right) {
         int ret = 0;
-        int l = 1, r = 0;
+        int l = -1, r = -1;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > right) l = i;
             if (nums[i] >= left) r = i;

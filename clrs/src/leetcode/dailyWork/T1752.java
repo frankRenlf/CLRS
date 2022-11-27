793bfb6b28584cf5803139d35aa930826a3f5281
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-27 10:57:23 +0800

    Update T1752.java

--- clrs/src/leetcode/dailyWork/T1752.java
+++ clrs/src/leetcode/dailyWork/T1752.java
@@ -16,7 +16,30 @@ public class T1752 {
 
     public boolean check(int[] nums) {
         int len = nums.length;
-        int x = nums[0] - 1;
+        int x = (len - nums[0] + 1) % len;
+        for (int i = 0; i < len; i++) {
+            if (nums[(i + x) % len] != i + 1) {
+                return false;
+            }
+        }
+        return true;
     }
-
+    public boolean check2(int[] nums) {
+        int n = nums.length, x = 0;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                x = i;
+                break;
+            }
+        }
+        if (x == 0) {
+            return true;
+        }
+        for (int i = x + 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return nums[0] >= nums[n - 1];
+    }
 }

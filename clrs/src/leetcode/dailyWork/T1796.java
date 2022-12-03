7c2449f2fd17b9a7f30439e0b7b96ecc577ffeb9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-03 11:09:36 +0800

    Update T1796.java

--- clrs/src/leetcode/dailyWork/T1796.java
+++ clrs/src/leetcode/dailyWork/T1796.java
@@ -15,7 +15,19 @@ package leetcode.dailyWork;
 public class T1796 {
 
     public int secondHighest(String s) {
-
+        int first = -1, second = -1;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                int val = c - '0';
+                if (first < val) {
+                    second = first;
+                    first = val;
+                } else if (val > second && val < first) {
+                    second = val;
+                }
+            }
+        }
+        return second;
     }
 
 }

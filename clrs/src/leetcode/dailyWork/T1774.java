80aa773dc4492f956a6f4d3928df9c21020dff73
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-04 11:06:29 +0800

    Update T1774.java

--- clrs/src/leetcode/dailyWork/T1774.java
+++ clrs/src/leetcode/dailyWork/T1774.java
@@ -19,28 +19,45 @@ public class T1774 {
 
     public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
         ret = Arrays.stream(baseCosts).min().getAsInt();
+        int len = toppingCosts.length;
         for (int c : baseCosts) {
-            dfs(toppingCosts, 0, toppingCosts.length, c, target);
+            dfs(toppingCosts, 0, len, c, target);
         }
         return ret;
     }
 
     private void dfs(int[] toppingCosts, int index, int length, int cur_cost, int target) {
-        int sub1 = Math.abs(ret - target), sub2 = Math.abs(cur_cost - target);
-        if (sub1 < sub2) {
+        if (Math.abs(ret - target) < cur_cost - target) {
             return;
-        } else {
-            if (sub1 > sub2) {
+        } else if (Math.abs(ret - target) >= Math.abs(cur_cost - target)) {
+            if (Math.abs(ret - target) > Math.abs(cur_cost - target)) {
                 ret = cur_cost;
             } else {
                 ret = Math.min(ret, cur_cost);
             }
         }
-
-        if (index >= length) return;
+        if (index == length) return;
         dfs(toppingCosts, index + 1, length, cur_cost + toppingCosts[index] * 2, target);
         dfs(toppingCosts, index + 1, length, cur_cost + toppingCosts[index], target);
         dfs(toppingCosts, index + 1, length, cur_cost, target);
+    }
+
+    public void dfs2(int[] toppingCosts, int p, int curCost, int target) {
+        if (Math.abs(ret - target) < curCost - target) {
+            return;
+        } else if (Math.abs(ret - target) >= Math.abs(curCost - target)) {
+            if (Math.abs(ret - target) > Math.abs(curCost - target)) {
+                ret = curCost;
+            } else {
+                ret = Math.min(ret, curCost);
+            }
+        }
+        if (p == toppingCosts.length) {
+            return;
+        }
+        dfs2(toppingCosts, p + 1, curCost + toppingCosts[p] * 2, target);
+        dfs2(toppingCosts, p + 1, curCost + toppingCosts[p], target);
+        dfs2(toppingCosts, p + 1, curCost, target);
     }
 
 

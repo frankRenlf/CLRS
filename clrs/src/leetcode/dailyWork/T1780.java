c76b9fc1c7378a9b09938b1ce2f92a8fa8e8bf18
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-09 10:23:32 +0800

    Create T1780.java

---
+++ clrs/src/leetcode/dailyWork/T1780.java
@@ -0,0 +1,27 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/9 10:19
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1780 {
+
+    public boolean checkPowersOfThree(int n) {
+        while (n != 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n %= 3;
+        }
+        return true;
+    }
+
+}

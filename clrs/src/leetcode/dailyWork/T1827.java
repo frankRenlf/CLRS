7db474c6621ca9584f670cb6f7b4f2579d7587f7
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-11 10:37:05 +0800

    Create T1827.java

---
+++ clrs/src/leetcode/dailyWork/T1827.java
@@ -0,0 +1,28 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/11 10:33
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1827 {
+
+    public int minOperations(int[] nums) {
+        int cnt = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                cnt += nums[i - 1] + 1 - nums[i];
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return cnt;
+    }
+
+}

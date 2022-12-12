2e90b12defb82ca658e4ffb0e89132f34de7db06
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-12 13:23:39 +0800

    Create T1781.java

---
+++ clrs/src/leetcode/dailyWork/T1781.java
@@ -0,0 +1,37 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/12 13:23
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1781 {
+
+    public int beautySum(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int[] cnt = new int[26];
+            int maxFreq = 0;
+            for (int j = i; j < s.length(); j++) {
+                cnt[s.charAt(j) - 'a']++;
+                maxFreq = Math.max(maxFreq, cnt[s.charAt(j) - 'a']);
+                int minFreq = s.length();
+                for (int k = 0; k < 26; k++) {
+                    if (cnt[k] > 0) {
+                        minFreq = Math.min(minFreq, cnt[k]);
+                    }
+                }
+                res += maxFreq - minFreq;
+            }
+        }
+        return res;
+    }
+
+}

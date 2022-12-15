33e2958dc2f199df0c746c7061b52fde2ee51571
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-15 12:52:52 +0800

    Create T1945.java

---
+++ clrs/src/leetcode/dailyWork/T1945.java
@@ -0,0 +1,21 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/15 12:52
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1945 {
+
+    public int getLucky(String s, int k) {
+        
+    }
+
+}

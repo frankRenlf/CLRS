a8b3ba2ee01fa130a4f335951946fd6164c4030e
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-15 13:10:42 +0800

    update

--- clrs/src/leetcode/dailyWork/T1945.java
+++ clrs/src/leetcode/dailyWork/T1945.java
@@ -15,7 +15,26 @@ package leetcode.dailyWork;
 public class T1945 {
 
     public int getLucky(String s, int k) {
-        
+        StringBuilder str = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            str.append(c - 'a' + 1);
+        }
+        while (str.length() > 1 && k > 0) {
+            int n = 0;
+            for (int i = 0; i < str.length(); i++) {
+                n += str.charAt(i) - '0';
+            }
+            str = new StringBuilder(Integer.toString(n));
+            k--;
+        }
+        return Integer.parseInt(str.toString());
+    }
+
+    static T1945 t1945 = new T1945();
+
+    public static void main(String[] args) {
+        System.out.println(t1945.getLucky("iiii", 1));
     }
 
 }
+

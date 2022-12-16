e76bf7f4bf2e419da6c443c172b0dd99a8786d55
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-16 10:00:11 +0800

    Create T1785.java

---
+++ clrs/src/leetcode/dailyWork/T1785.java
@@ -0,0 +1,26 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/16 9:59
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1785 {
+
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        long diff = Math.abs(sum - goal);
+        return (int) ((diff + limit - 1) / limit);
+    }
+
+}

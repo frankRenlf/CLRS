476d7b86142d97cadc357b19662de2f6bdfb4adf
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-20 14:03:25 +0800

    Create T1760.java

---
+++ clrs/src/leetcode/dailyWork/T1760.java
@@ -0,0 +1,37 @@
+package leetcode.dailyWork;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/20 14:03
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1760 {
+
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1, right = Arrays.stream(nums).max().getAsInt();
+        int ans = 0;
+        while (left <= right) {
+            int y = (left + right) / 2;
+            long ops = 0;
+            for (int x : nums) {
+                ops += (x - 1) / y;
+            }
+            if (ops <= maxOperations) {
+                ans = y;
+                right = y - 1;
+            } else {
+                left = y + 1;
+            }
+        }
+        return ans;
+    }
+}

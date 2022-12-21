34aec8abe55af14ed008766ef1d340ccb8b07c54
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-21 10:15:36 +0800

    Create T1753.java

---
+++ clrs/src/leetcode/dailyWork/T1753.java
@@ -0,0 +1,23 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/21 10:15
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1753 {
+
+    public int maximumScore(int a, int b, int c) {
+        int sum = a + b + c;
+        int maxVal = Math.max(Math.max(a, b), c);
+        return Math.min(sum - maxVal, sum / 2);
+    }
+
+}

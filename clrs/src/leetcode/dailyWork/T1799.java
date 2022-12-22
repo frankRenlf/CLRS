6f17d6b52443f18aa733f76c21ed80cd85a9f242
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-22 13:35:13 +0800

    Create T1799.java

---
+++ clrs/src/leetcode/dailyWork/T1799.java
@@ -0,0 +1,54 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/22 13:34
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1799 {
+
+    public int maxScore(int[] nums) {
+        int m = nums.length;
+        int[] dp = new int[1 << m];
+        int[][] gcdTmp = new int[m][m];
+        for (int i = 0; i < m; ++i) {
+            for (int j = i + 1; j < m; ++j) {
+                gcdTmp[i][j] = gcd(nums[i], nums[j]);
+            }
+        }
+        int all = 1 << m;
+        for (int s = 1; s < all; ++s) {
+            int t = Integer.bitCount(s);
+            if ((t & 1) != 0) {
+                continue;
+            }
+            for (int i = 0; i < m; ++i) {
+                if (((s >> i) & 1) != 0) {
+                    for (int j = i + 1; j < m; ++j) {
+                        if (((s >> j) & 1) != 0) {
+                            dp[s] = Math.max(dp[s], dp[s ^ (1 << i) ^ (1 << j)] + t / 2 * gcdTmp[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[all - 1];
+    }
+
+    public int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp % num2;
+        }
+        return num1;
+    }
+
+}

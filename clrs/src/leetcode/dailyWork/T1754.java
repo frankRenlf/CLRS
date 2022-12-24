8f6791f7a3f04e5ce8e8cff62d0674285f4828b0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-24 09:34:27 +0800

    Create T1754.java

---
+++ clrs/src/leetcode/dailyWork/T1754.java
@@ -0,0 +1,32 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/24 9:34
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1754 {
+
+    public String largestMerge(String word1, String word2) {
+        StringBuilder merge = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length() && word1.substring(i).compareTo(word2.substring(j)) > 0) {
+                merge.append(word1.charAt(i));
+                i++;
+            } else {
+                merge.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return merge.toString();
+    }
+
+}

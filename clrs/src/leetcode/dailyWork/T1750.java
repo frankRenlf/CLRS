a7f10d5ad89b00caf2b55cc31cc5544112289de7
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-28 10:26:00 +0800

    Create T1750.java

---
+++ clrs/src/leetcode/dailyWork/T1750.java
@@ -0,0 +1,21 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/28 10:25
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1750 {
+
+    public int minimumLength(String s) {
+        
+    }
+
+}

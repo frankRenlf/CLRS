035c95a73bf5131d41393eed1ba16ca37c08bbe0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-30 10:14:25 +0800

    Create T855.java

---
+++ clrs/src/leetcode/dailyWork/T855.java
@@ -0,0 +1,35 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2022/12/30 10:11
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+
+class ExamRoom {
+
+    int[] list;
+
+    public ExamRoom(int n) {
+        this.list = new int[n];
+    }
+
+    public int seat() {
+
+    }
+
+    public void leave(int p) {
+
+    }
+}
+
+
+public class T855 {
+}

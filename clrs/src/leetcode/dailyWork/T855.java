d1b278edd02feb787daa7cb436c9adbcb4f79585
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-30 10:22:26 +0800

    Update T855.java

--- clrs/src/leetcode/dailyWork/T855.java
+++ clrs/src/leetcode/dailyWork/T855.java
@@ -1,5 +1,7 @@
 package leetcode.dailyWork;
 
+import java.util.TreeSet;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,19 +16,50 @@ package leetcode.dailyWork;
  */
 
 class ExamRoom {
+    int N;
+    TreeSet<Integer> students;
 
-    int[] list;
-
-    public ExamRoom(int n) {
-        this.list = new int[n];
+    public ExamRoom(int N) {
+        this.N = N;
+        students = new TreeSet<>();
     }
 
     public int seat() {
+        //Let's determine student, the position of the next
+        //student to sit down.
+        int student = 0;
+        if (students.size() > 0) {
+            //Tenatively, dist is the distance to the closest student,
+            //which is achieved by sitting in the position 'student'.
+            //We start by considering the left-most seat.
+            int dist = students.first();
+            Integer prev = null;
+            for (Integer s : students) {
+                if (prev != null) {
+                    //For each pair of adjacent students in positions (prev, s),
+                    //d is the distance to the closest student;
+                    //achieved at position prev + d.
+                    int d = (s - prev) / 2;
+                    if (d > dist) {
+                        dist = d;
+                        student = prev + d;
+                    }
+                }
+                prev = s;
+            }
 
+            //Considering the right-most seat.
+            if (N - 1 - students.last() > dist)
+                student = N - 1;
+        }
+
+        //Add the student to our sorted TreeSet of positions.
+        students.add(student);
+        return student;
     }
 
     public void leave(int p) {
-
+        students.remove(p);
     }
 }
 

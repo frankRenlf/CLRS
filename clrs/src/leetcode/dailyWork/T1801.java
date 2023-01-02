f0b9cac906e6940f685233dba981a9bb7179fe4b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-02 13:43:26 +0800

    Create T1801.java

---
+++ clrs/src/leetcode/dailyWork/T1801.java
@@ -0,0 +1,61 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/1/2 13:43
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1801 {
+
+    public int getNumberOfBacklogOrders(int[][] orders) {
+        final int MOD = 1000000007;
+        PriorityQueue<int[]> buyOrders = new PriorityQueue<int[]>((a, b) -> b[0] - a[0]);
+        PriorityQueue<int[]> sellOrders = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        for (int[] order : orders) {
+            int price = order[0], amount = order[1], orderType = order[2];
+            if (orderType == 0) {
+                while (amount > 0 && !sellOrders.isEmpty() && sellOrders.peek()[0] <= price) {
+                    int[] sellOrder = sellOrders.poll();
+                    int sellAmount = Math.min(amount, sellOrder[1]);
+                    amount -= sellAmount;
+                    sellOrder[1] -= sellAmount;
+                    if (sellOrder[1] > 0) {
+                        sellOrders.offer(sellOrder);
+                    }
+                }
+                if (amount > 0) {
+                    buyOrders.offer(new int[]{price, amount});
+                }
+            } else {
+                while (amount > 0 && !buyOrders.isEmpty() && buyOrders.peek()[0] >= price) {
+                    int[] buyOrder = buyOrders.poll();
+                    int buyAmount = Math.min(amount, buyOrder[1]);
+                    amount -= buyAmount;
+                    buyOrder[1] -= buyAmount;
+                    if (buyOrder[1] > 0) {
+                        buyOrders.offer(buyOrder);
+                    }
+                }
+                if (amount > 0) {
+                    sellOrders.offer(new int[]{price, amount});
+                }
+            }
+        }
+        int total = 0;
+        for (PriorityQueue<int[]> pq : Arrays.asList(buyOrders, sellOrders)) {
+            while (!pq.isEmpty()) {
+                int[] order = pq.poll();
+                total = (total + order[1]) % MOD;
+            }
+        }
+        return total;
+    }
+
+}

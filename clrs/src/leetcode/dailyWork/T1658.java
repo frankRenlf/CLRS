bf49b5d1fd1a690f07d93c336463b4ad25427391
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-07 12:34:43 +0800

    Update T1658.java

--- clrs/src/leetcode/dailyWork/T1658.java
+++ clrs/src/leetcode/dailyWork/T1658.java
@@ -15,12 +15,32 @@ package leetcode.dailyWork;
 public class T1658 {
     public int minOperations(int[] nums, int x) {
         int len = nums.length;
-        int[] sum = new int[len];
-        sum[0] = nums[0];
+        int[] left = new int[len];
+        left[0] = nums[0];
         for (int i = 1; i < len; i++) {
-            sum[i] += sum[i - 1] + nums[i];
+            left[i] += left[i - 1] + nums[i];
         }
-        if (sum[len - 1] < x) return -1;
-        
+        if (left[len - 1] < x) return -1;
+        if (left[len - 1] == x) return len;
+        int[] right = new int[len];
+        right[len - 1] = nums[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            right[i] += right[i + 1] + nums[i];
+        }
+        int ret = len;
+        boolean k = false;
+        for (int r = 1; r <= len; r++) {
+            int l = -1;
+            int sum = r==len?0:right[r];
+            while (sum < x) {
+                l++;
+                sum = left[l] + (r == len ? 0 : right[r]);
+            }
+            if (sum == x) {
+                k = true;
+                ret = Math.min(l + 1 + len - r, ret);
+            }
+        }
+        return k ? ret : -1;
     }
 }

4b371b9979a17b4de7c621f01dc2321d26420534
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-21 09:20:10 +0800

    Create T1824.java

---
+++ clrs/src/leetcode/dailyWork/T1824.java
@@ -0,0 +1,43 @@
+package leetcode.dailyWork;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/1/21 9:19
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1824 {
+    static final int INF = 0x3fffffff;
+
+    public int minSideJumps(int[] obstacles) {
+        int n = obstacles.length - 1;
+        int[] d = new int[3];
+        Arrays.fill(d, 1);
+        d[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            int minCnt = INF;
+            for (int j = 0; j < 3; j++) {
+                if (j == obstacles[i] - 1) {
+                    d[j] = INF;
+                } else {
+                    minCnt = Math.min(minCnt, d[j]);
+                }
+            }
+            for (int j = 0; j < 3; j++) {
+                if (j == obstacles[i] - 1) {
+                    continue;
+                }
+                d[j] = Math.min(d[j], minCnt + 1);
+            }
+        }
+        return Math.min(Math.min(d[0], d[1]), d[2]);
+    }
+}

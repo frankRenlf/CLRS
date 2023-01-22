0653f232606ccdedae1e4cc5e0623c1e147f53f1
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-22 09:40:17 +0800

    Create T1815.java

---
+++ clrs/src/leetcode/dailyWork/T1815.java
@@ -0,0 +1,65 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/1/22 9:40
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1815 {
+
+    static final int K_WIDTH = 5;
+    static final int K_WIDTH_MASK = (1 << K_WIDTH) - 1;
+
+    public int maxHappyGroups(int batchSize, int[] groups) {
+        int[] cnt = new int[batchSize];
+        for (int x : groups) {
+            ++cnt[x % batchSize];
+        }
+
+        long start = 0;
+        for (int i = batchSize - 1; i >= 1; --i) {
+            start = (start << K_WIDTH) | cnt[i];
+        }
+
+        Map<Long, Integer> memo = new HashMap<Long, Integer>();
+
+        return dfs(memo, batchSize, start) + cnt[0];
+    }
+
+    public int dfs(Map<Long, Integer> memo, int batchSize, long mask) {
+        if (mask == 0) {
+            return 0;
+        }
+
+        if (!memo.containsKey(mask)) {
+            long total = 0;
+            for (int i = 1; i < batchSize; ++i) {
+                long amount = ((mask >> ((i - 1) * K_WIDTH)) & K_WIDTH_MASK);
+                total += i * amount;
+            }
+
+            int best = 0;
+            for (int i = 1; i < batchSize; ++i) {
+                long amount = ((mask >> ((i - 1) * K_WIDTH)) & K_WIDTH_MASK);
+                if (amount > 0) {
+                    int result = dfs(memo, batchSize, mask - (1L << ((i - 1) * K_WIDTH)));
+                    if ((total - i) % batchSize == 0) {
+                        ++result;
+                    }
+                    best = Math.max(best, result);
+                }
+            }
+
+            memo.put(mask, best);
+        }
+        return memo.get(mask);
+    }
+
+}

df77dd11e58dd459d75027c3b683fae4b48eabe4
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-26 09:47:46 +0800

    add

--- clrs/src/leetcode/dailyWork/T1663.java
+++ clrs/src/leetcode/dailyWork/T1663.java
@@ -1,5 +1,7 @@
 package leetcode.dailyWork;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -15,7 +17,31 @@ package leetcode.dailyWork;
 public class T1663 {
 
     public String getSmallestString(int n, int k) {
+        int[] arr = new int[n];
+        Arrays.fill(arr, 1);
+        int sum = n;
+        int right = n - 1;
+        while (sum < k && right >= 0) {
+            int sub = k - sum;
+            if (sub > 25) {
+                arr[right] = 26;
+                right--;
+            } else {
+                arr[right] = sub;
+                break;
+            }
+        }
+        StringBuilder ret = new StringBuilder();
+        for (int x : arr) {
+            ret.append('a' + x - 1);
+        }
+        return ret.toString();
+    }
 
+    static T1663 t1663 = new T1663();
+
+    public static void main(String[] args) {
+        System.out.println(t1663.getSmallestString(3, 27));
     }
 
 }

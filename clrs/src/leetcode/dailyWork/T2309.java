4a3afbf08389fa57d3de708c224b288fcaf839e9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-27 22:10:20 +0800

    Create T2309.java

---
+++ clrs/src/leetcode/dailyWork/T2309.java
@@ -0,0 +1,19 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/1/27 22:10
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T2309 {
+
+    
+
+}

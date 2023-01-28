db76161f2b29751cde8e0bf95088c44eedb00334
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-28 09:50:45 +0800

    add

--- clrs/src/leetcode/dailyWork/T1664.java
+++ clrs/src/leetcode/dailyWork/T1664.java
@@ -15,18 +15,36 @@ package leetcode.dailyWork;
 public class T1664 {
 
     public int waysToMakeFair(int[] nums) {
-        int n = nums.length;
-        int oddVal = 0, evenVal = 0;
-        int[] odd = new int[n];
-        int[] even = new int[n];
-        for (int i = n-1; i >=0; i--) {
-            if (i % 2 == 0) {
-                evenVal += nums[i];
+        // 预处理 x为偶数下标和 y为奇数下标和
+        int x = 0, y = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if ((i & 1) == 0) {
+                x += nums[i];
             } else {
-                oddVal += nums[i];
+                y += nums[i];
             }
         }
+        // pre_x 不受下标改变的偶数和 pre_y 同理
+        int ans = 0, pre_x = 0, pre_y = 0;
+        // 枚举每一个删除的下标
+        for (int i = 0; i < n; i++) {
+            if ((i & 1) == 0) {
+                x -= nums[i];
+            } else {
+                y -= nums[i];
+            }
+            // 删除后，后续数字的下标发生转换
+            if (y + pre_x == x + pre_y) ans++;
 
+            // 当前数字在后续的删除中不受下标改变
+            if ((i & 1) == 0) {
+                pre_x += nums[i];
+            } else {
+                pre_y += nums[i];
+            }
+
+        }
+        return ans;
     }
 
 }

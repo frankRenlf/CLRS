e1d6aadb8452de13d33c9ea92c2ba2db87ab4f10
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-29 17:17:35 +0800

    Update T2315.java

--- clrs/src/leetcode/dailyWork/T2315.java
+++ clrs/src/leetcode/dailyWork/T2315.java
@@ -13,4 +13,19 @@ package leetcode.dailyWork;
  * @Description :
  */
 public class T2315 {
+
+    public int countAsterisks(String s) {
+        int count = 0;
+        int ret = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '|') {
+                count++;
+            }
+            if (c == '*' && (count & 2) == 0) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+
 }

17cdbe274b93eef7fa41de07f782a75349db5441
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-30 22:17:48 +0800

    Update T1669.java

--- clrs/src/leetcode/dailyWork/T1669.java
+++ clrs/src/leetcode/dailyWork/T1669.java
@@ -1,5 +1,7 @@
 package leetcode.dailyWork;
 
+import leetcode.algorithm.ListNode;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,4 +15,21 @@ package leetcode.dailyWork;
  * @Description :
  */
 public class T1669 {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode l2End = list2;
+        while (l2End.next != null) {
+            l2End = l2End.next;
+        }
+        ListNode l1Pre = list1;
+        for (int i = 0; i < a - 1; i++) {
+            l1Pre = l1Pre.next;
+        }
+        ListNode l1End = l1Pre;
+        for (int i = 0; i < b - a + 2; i++) {
+            l1End = l1End.next;
+        }
+        l1Pre.next = list2;
+        l2End.next = l1End;
+        return list1;
+    }
 }

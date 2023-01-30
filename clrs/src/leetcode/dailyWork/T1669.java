0498b5be07572045fcbd54bd8305a5f049f2df07
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-30 22:08:49 +0800

    Create T1669.java

---
+++ clrs/src/leetcode/dailyWork/T1669.java
@@ -0,0 +1,13 @@
+package leetcode.dailyWork;/**
+ * Created with IntelliJ IDEA.
+ * @Project      : CLRS
+ * @Package      : leetcode.dailyWork
+ * @createTime   : 2023/1/30 22:08
+ * @version      : 1.0
+ * @author       : Frank.Ren
+ * @Email        : sc19lr@leeds.ac.uk
+ * @github       : https://github.com/frankRenlf
+ * @Description  : 
+ */
+public class T1669 {
+}

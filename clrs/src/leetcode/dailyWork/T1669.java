2cc558c179de756b32984ec63dc9642c4e29b3d8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-30 22:09:01 +0800

    Update T1669.java

--- clrs/src/leetcode/dailyWork/T1669.java
+++ clrs/src/leetcode/dailyWork/T1669.java
@@ -1,13 +1,16 @@
-package leetcode.dailyWork;/**
+package leetcode.dailyWork;
+
+/**
  * Created with IntelliJ IDEA.
- * @Project      : CLRS
- * @Package      : leetcode.dailyWork
- * @createTime   : 2023/1/30 22:08
- * @version      : 1.0
- * @author       : Frank.Ren
- * @Email        : sc19lr@leeds.ac.uk
- * @github       : https://github.com/frankRenlf
- * @Description  : 
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/1/30 22:08
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
  */
 public class T1669 {
 }

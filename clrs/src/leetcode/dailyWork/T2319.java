221f1a6322b2a47d9774acf4ec13510e1d3706f4
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-31 20:51:46 +0800

    add

--- clrs/src/leetcode/dailyWork/T2319.java
+++ clrs/src/leetcode/dailyWork/T2319.java
@@ -15,7 +15,21 @@ package leetcode.dailyWork;
 public class T2319 {
 
     public boolean checkXMatrix(int[][] grid) {
-
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i == n - j) {
+                    if (grid[i][j] == 0) {
+                        return false;
+                    }
+                } else {
+                    if (grid[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
 }

c8d280db1da6ad9bf8b5392a11d4d1551f3dc3de
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-01 20:14:10 +0800

    add

---
+++ clrs/src/leetcode/dailyWork/T2325.java
@@ -0,0 +1,37 @@
+package leetcode.dailyWork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/1 20:09
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T2325 {
+
+    public String decodeMessage(String key, String message) {
+        Map<Character, Character> map = new HashMap<>();
+        char a = 'a';
+        for (char c :
+                key.toCharArray()) {
+            if (c!=' '&&!map.containsKey(c)) {
+                map.put(c, a++);
+            }
+        }
+        StringBuilder ret = new StringBuilder();
+        for (char c :
+                message.toCharArray()) {
+            ret.append(map.getOrDefault(c,' '));
+        }
+        return ret.toString();
+    }
+
+}

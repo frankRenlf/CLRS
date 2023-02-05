3acd7678f51434c98ce01357fc40666a920d404c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-05 17:05:48 +0800

    Update T200.java

--- clrs/src/leetcode/dailyWork/T200.java
+++ clrs/src/leetcode/dailyWork/T200.java
@@ -14,6 +14,29 @@ package leetcode.dailyWork;
  */
 public class T200 {
 
-    
+    public int numIslands(char[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j, n, m);
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+
+    private void dfs(char[][] grid, int i, int j, int n, int m) {
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i, j + 1, n, m);
+        dfs(grid, i - 1, j, n, m);
+        dfs(grid, i, j - 1, n, m);
+        dfs(grid, i + 1, j, n, m);
+    }
 
 }

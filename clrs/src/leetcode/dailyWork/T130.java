5b96d5561837857a6d73fa1df6d10b256758ba15
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-05 17:02:20 +0800

    update

--- clrs/src/leetcode/dailyWork/T130.java
+++ clrs/src/leetcode/dailyWork/T130.java
@@ -15,7 +15,36 @@ package leetcode.dailyWork;
 public class T130 {
 
     public void solve(char[][] board) {
-        
+        int n = board.length;
+        int m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            dfs(board, i, 0, n, m);
+            dfs(board, i, m - 1, n, m);
+        }
+        for (int i = 0; i < m; i++) {
+            dfs(board, 0, i, n, m);
+            dfs(board, n - 1, i, n, m);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j, int n, int m) {
+        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'A';
+        dfs(board, i - 1, j, n, m);
+        dfs(board, i, j + 1, n, m);
+        dfs(board, i + 1, j, n, m);
+        dfs(board, i, j - 1, n, m);
     }
 
 }
---
+++ clrs/src/leetcode/dailyWork/T200.java
@@ -0,0 +1,19 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/5 17:02
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T200 {
+
+    
+
+}

c28888dc150fa131302f79876eda921295d85a51
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-06 18:06:49 +0800

    Create T2331.java

---
+++ clrs/src/leetcode/dailyWork/T2331.java
@@ -0,0 +1,37 @@
+package leetcode.dailyWork;
+
+import leetcode.algorithm.TreeNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/6 17:59
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T2331 {
+
+    public boolean evaluateTree(TreeNode root) {
+        Boolean ret = null;
+        if (root.val == 2) {
+            ret = evaluateTree(root.left) || evaluateTree(root.right);
+        } else if (root.val == 3) {
+            ret = evaluateTree(root.left) && evaluateTree(root.right);
+        } else {
+            return root.val == 0;
+        }
+        return ret;
+    }
+
+    static T2331 t2331 = new T2331();
+
+    public static void main(String[] args) {
+        System.out.println(t2331.());
+    }
+
+}

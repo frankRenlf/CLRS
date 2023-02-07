d8864ee8ac1719ff2b26e8da6fad3053fff23347
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-07 10:32:46 +0800

    Update T1604.java

--- clrs/src/leetcode/dailyWork/T1604.java
+++ clrs/src/leetcode/dailyWork/T1604.java
@@ -1,5 +1,7 @@
 package leetcode.dailyWork;
 
+import java.util.*;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,4 +15,29 @@ package leetcode.dailyWork;
  * @Description :
  */
 public class T1604 {
+    public List<String> alertNames(String[] keyName, String[] keyTime) {
+        Map<String, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < keyName.length; ++i) {
+            String name = keyName[i];
+            String time = keyTime[i];
+            int t = Integer.parseInt(time.substring(0, 2)) * 60 + Integer.parseInt(time.substring(3));
+            map.computeIfAbsent(name, k -> new ArrayList<>()).add(t);
+        }
+        List<String> ans = new ArrayList<>();
+        for (Map.Entry<String, List<Integer>> el : map.entrySet()) {
+            List<Integer> count = el.getValue();
+            int n = count.size();
+            if (n > 2) {
+                Collections.sort(count);
+                for (int i = 0; i < n - 2; ++i) {
+                    if (count.get(i + 2) - count.get(i) <= 60) {
+                        ans.add(el.getKey());
+                        break;
+                    }
+                }
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
 }

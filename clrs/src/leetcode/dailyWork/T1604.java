8c0974bacd0950f4ce37c6c1c1df0329df5c7f34
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-07 10:18:08 +0800

    Update T1604.java

--- clrs/src/leetcode/dailyWork/T1604.java
+++ clrs/src/leetcode/dailyWork/T1604.java
@@ -9,7 +9,7 @@ package leetcode.dailyWork;
  * @Package : leetcode.dailyWork
  * @createTime : 2023/2/7 10:17
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T1604 {

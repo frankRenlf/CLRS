d06779568f030fce1428a6c8e2bcf3c7c6cfec44
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-08 11:34:39 +0800

    Update T1233.java

--- clrs/src/leetcode/dailyWork/T1233.java
+++ clrs/src/leetcode/dailyWork/T1233.java
@@ -1,6 +1,6 @@
 package leetcode.dailyWork;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +16,17 @@ import java.util.List;
  */
 public class T1233 {
     public List<String> removeSubfolders(String[] folder) {
-
+        Set<String> set = new HashSet<>(Arrays.asList(folder));
+        List<String> list = new ArrayList<>();
+        for (String s : folder) {
+            int i = s.length() - 1;
+            for (; i > 0; i--) {
+                if (s.charAt(i) == '/' && set.contains(s.substring(0, i))) {
+                    break;
+                }
+            }
+            if (i == 0) list.add(s);
+        }
+        return list;
     }
 }

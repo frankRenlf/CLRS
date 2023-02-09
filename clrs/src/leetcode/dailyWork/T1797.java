d0b14db2ff776a11405507bd836b8cf5ea72f7fc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-09 10:24:41 +0800

    add

--- clrs/src/leetcode/dailyWork/T1797.java
+++ clrs/src/leetcode/dailyWork/T1797.java
@@ -1,5 +1,8 @@
 package leetcode.dailyWork;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -15,23 +18,45 @@ package leetcode.dailyWork;
 
 
 class AuthenticationManager {
+    int timeToLive;
+    Map<String, Integer> map;
 
     public AuthenticationManager(int timeToLive) {
-
+        this.timeToLive = timeToLive;
+        map = new HashMap<>();
     }
 
     public void generate(String tokenId, int currentTime) {
-
+        map.put(tokenId, this.timeToLive + currentTime);
     }
 
     public void renew(String tokenId, int currentTime) {
-
+        Integer until = map.get(tokenId);
+        if (until != null && until > currentTime) {
+            map.put(tokenId, currentTime + this.timeToLive);
+        }
     }
 
     public int countUnexpiredTokens(int currentTime) {
-
+        int cnt = 0;
+        for (Map.Entry<String, Integer> el : map.entrySet()) {
+            if (el.getValue() > currentTime) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
 
 public class T1797 {
+    public static void main(String[] args) {
+        AuthenticationManager authenticationManager = new AuthenticationManager(5);
+        authenticationManager.renew("aaa", 1);
+        authenticationManager.generate("aaa", 2);
+        System.out.println(authenticationManager.countUnexpiredTokens(6));
+        authenticationManager.generate("bbb", 7);
+        authenticationManager.renew("aaa", 8);
+        authenticationManager.renew("bbb", 10);
+        authenticationManager.countUnexpiredTokens(15);
+    }
 }

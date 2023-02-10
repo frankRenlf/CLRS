630136996ac8e5fccc53c1c7fe5ecf425b9e9c39
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-10 08:54:46 +0800

    Update T1223.java

--- clrs/src/leetcode/dailyWork/T1223.java
+++ clrs/src/leetcode/dailyWork/T1223.java
@@ -9,7 +9,7 @@ package leetcode.dailyWork;
  * @Package : leetcode.dailyWork
  * @createTime : 2023/2/10 8:52
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T1223 {
@@ -33,6 +33,6 @@ public class T1223 {
         }
         return sum[n];
     }
-    
+
 
 }

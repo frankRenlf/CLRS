36860567d1d5aa6d782c21172329a1ab0708165a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-11 08:49:23 +0800

    Update T2335.java

--- clrs/src/leetcode/dailyWork/T2335.java
+++ clrs/src/leetcode/dailyWork/T2335.java
@@ -1,5 +1,7 @@
 package leetcode.dailyWork;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -15,7 +17,26 @@ package leetcode.dailyWork;
 public class T2335 {
 
     public int fillCups(int[] amount) {
-        
+        int sum = Arrays.stream(amount).sum();
+        Integer key = null;
+        for (int el : amount) {
+            if (sum < 2 * el) {
+                key = el;
+                break;
+            }
+        }
+        if (key != null) {
+            return key;
+        }
+        return sum % 2 == 0 ? sum / 2 : sum / 2 + 1;
     }
+    public int fillCups2(int[] amount) {
+        Arrays.sort(amount);
+        if (amount[2] > amount[1] + amount[0]) {
+            return amount[2];
+        }
+        return (amount[0] + amount[1] + amount[2] + 1) / 2;
+    }
+
 
 }

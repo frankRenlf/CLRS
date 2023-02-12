56769168597e6c2797d792920ddf86e023d10dff
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-12 10:07:38 +0800

    Update T1138.java

--- clrs/src/leetcode/dailyWork/T1138.java
+++ clrs/src/leetcode/dailyWork/T1138.java
@@ -9,7 +9,7 @@ package leetcode.dailyWork;
  * @Package : leetcode.dailyWork
  * @createTime : 2023/2/12 9:46
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T1138 {

8dab724617977574291c7ffaeb9467b212801d36
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-14 09:15:01 +0800

    Create T1124.java

---
+++ clrs/src/leetcode/dailyWork/T1124.java
@@ -0,0 +1,19 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/14 9:14
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1124 {
+
+
+
+}

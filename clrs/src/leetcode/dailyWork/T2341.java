d952a11989e1f18fb099e75ad94e5a37ffa740e6
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-16 10:24:16 +0800

    Update T2341.java

--- clrs/src/leetcode/dailyWork/T2341.java
+++ clrs/src/leetcode/dailyWork/T2341.java
@@ -9,7 +9,7 @@ package leetcode.dailyWork;
  * @Package : leetcode.dailyWork
  * @createTime : 2023/2/16 10:23
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T2341 {

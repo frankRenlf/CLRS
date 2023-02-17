68503def62edbf5f03170fa6f52e5531b0d7523a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-17 10:13:49 +0800

    Update T1139.java

--- clrs/src/leetcode/dailyWork/T1139.java
+++ clrs/src/leetcode/dailyWork/T1139.java
@@ -13,5 +13,25 @@ package leetcode.dailyWork;
  * @Description :
  */
 public class T1139 {
-    
+    public int largest1BorderedSquare(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int[][] left = new int[n + 1][m + 1];
+        int[][] up = new int[n + 1][m + 1];
+        int maxBorder = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (grid[i - 1][j - 1] == 1) {
+                    left[i][j] = left[i][j - 1] + 1;
+                    up[i][j] = up[i - 1][j] + 1;
+                    int curBorder = Math.min(left[i][j], up[i][j]);
+                    while (curBorder > left[i - curBorder + 1][j]
+                            || curBorder > up[i][j - curBorder + 1]) {
+                        curBorder--;
+                    }
+                    maxBorder = Math.max(maxBorder, curBorder);
+                }
+            }
+        }
+        return maxBorder * maxBorder;
+    }
 }

578aa1c859b0f39deb107677dcec49a15d7319b3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-18 18:44:59 +0800

    Create T1237.java

---
+++ clrs/src/leetcode/dailyWork/T1237.java
@@ -0,0 +1,25 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/18 18:44
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+
+class CustomFunction {
+    // Returns f(x, y) for any given positive integers x and y.
+    // Note that f(x, y) is increasing with respect to both x and y.
+    // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+    public int f(int x, int y);
+};
+
+public class T1237 {
+
+}

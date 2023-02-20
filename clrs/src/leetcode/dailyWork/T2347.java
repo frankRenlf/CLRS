6901cfe136394b6c73a8ae6e6342c98886694177
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-20 12:16:42 +0800

    Create T2347.java

---
+++ clrs/src/leetcode/dailyWork/T2347.java
@@ -0,0 +1,16 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/20 11:00
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T2347 {
+}

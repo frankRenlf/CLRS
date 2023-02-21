641d9c1c0702f94e071173a4f326471ed6f368b9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-21 10:33:27 +0800

    update

--- clrs/src/leetcode/dailyWork/T1326.java
+++ clrs/src/leetcode/dailyWork/T1326.java
@@ -16,23 +16,25 @@ import java.util.Arrays;
  */
 public class T1326 {
     public int minTaps(int n, int[] ranges) {
-        int[][] intervals = new int[n + 1][];
+        int[][] gaps = new int[n + 1][2];
         for (int i = 0; i <= n; i++) {
             int start = Math.max(0, i - ranges[i]);
             int end = Math.min(n, i + ranges[i]);
-            intervals[i] = new int[]{start, end};
+            gaps[i] = new int[]{start, end};
         }
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Arrays.sort(gaps, (a, b) -> a[0] - b[0]);
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for (int[] interval : intervals) {
-            int start = interval[0], end = interval[1];
+        for (int[] gap :
+                gaps) {
+            int start = gap[0];
+            int end = gap[1];
             if (dp[start] == Integer.MAX_VALUE) {
                 return -1;
             }
-            for (int j = start; j <= end; j++) {
-                dp[j] = Math.min(dp[j], dp[start] + 1);
+            for (int i = start; i <= end; i++) {
+                dp[i] = Math.min(dp[i], dp[start] + 1);
             }
         }
         return dp[n];
@@ -41,6 +43,7 @@ public class T1326 {
     static T1326 t1326 = new T1326();
 
     public static void main(String[] args) {
-        System.out.println(t1326.minTaps(5, new int[]{3, 4, 1, 1, 0, 0}));
+        System.out.println(t1326.minTaps(8, new int[]{
+                4, 0, 0, 0, 0, 0, 0, 0, 4}));
     }
 }

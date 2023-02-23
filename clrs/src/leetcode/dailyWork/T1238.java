6d46ba8c0c23a4a8c4b083e572632b35653e2058
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-23 10:29:29 +0800

    Create T1238.java

---
+++ clrs/src/leetcode/dailyWork/T1238.java
@@ -0,0 +1,21 @@
+package leetcode.dailyWork;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/23 10:28
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1238 {
+    public List<Integer> circularPermutation(int n, int start) {
+
+    }
+}

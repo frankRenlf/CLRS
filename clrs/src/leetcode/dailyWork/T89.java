77de367f63720551158ddeda9b1bbcc99476a1e1
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-23 10:50:15 +0800

    Update T89.java

--- clrs/src/leetcode/dailyWork/T89.java
+++ clrs/src/leetcode/dailyWork/T89.java
@@ -9,7 +9,7 @@ package leetcode.dailyWork;
  * @Package : leetcode.dailyWork
  * @createTime : 2023/2/23 10:50
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T89 {

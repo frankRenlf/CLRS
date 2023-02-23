6f9f863917c44a9b834331fb72e192646e3ea9a1
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-23 10:50:06 +0800

    Create T89.java

---
+++ clrs/src/leetcode/dailyWork/T89.java
@@ -0,0 +1,27 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/23 10:50
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T89 {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ret = new ArrayList<Integer>();
+        ret.add(0);
+        for (int i = 1; i <= n; i++) {
+            int m = ret.size();
+            for (int j = m - 1; j >= 0; j--) {
+                ret.add(ret.get(j) | (1 << (i - 1)));
+            }
+        }
+        return ret;
+    }
+}

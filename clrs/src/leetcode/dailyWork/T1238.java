0f810b5370903f4d8a007873e75308227edd0211
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-23 10:31:28 +0800

    Update T1238.java

--- clrs/src/leetcode/dailyWork/T1238.java
+++ clrs/src/leetcode/dailyWork/T1238.java
@@ -1,5 +1,6 @@
 package leetcode.dailyWork;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +17,14 @@ import java.util.List;
  */
 public class T1238 {
     public List<Integer> circularPermutation(int n, int start) {
-
+        List<Integer> ret = new ArrayList<Integer>();
+        ret.add(start);
+        for (int i = 1; i <= n; i++) {
+            int m = ret.size();
+            for (int j = m - 1; j >= 0; j--) {
+                ret.add(((ret.get(j) ^ start) | (1 << (i - 1))) ^ start);
+            }
+        }
+        return ret;
     }
 }

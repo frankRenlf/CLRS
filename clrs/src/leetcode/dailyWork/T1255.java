77786063791ef5a905709cc045c82e464b3ffee6
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-26 10:03:24 +0800

    Create T1255.java

---
+++ clrs/src/leetcode/dailyWork/T1255.java
@@ -0,0 +1,19 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/26 10:03
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1255 {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+
+    }
+}

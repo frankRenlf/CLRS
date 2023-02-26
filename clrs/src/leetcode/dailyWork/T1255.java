e366918e9117f0597d985a89ee13b784767d3db8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-26 10:13:46 +0800

    update

--- clrs/src/leetcode/dailyWork/T1255.java
+++ clrs/src/leetcode/dailyWork/T1255.java
@@ -1,5 +1,8 @@
 package leetcode.dailyWork;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +17,40 @@ package leetcode.dailyWork;
  */
 public class T1255 {
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int n = words.length, res = 0;
+        int[] count = new int[26];
+        for (char c : letters) {
+            count[c - 'a']++;
+        }
+        for (int s = 1; s < (1 << n); s++) {
+            int[] wordCount = new int[26]; // 统计子集 s 所有单词的字母数目
+            for (int k = 0; k < n; k++) {
+                if ((s & (1 << k)) == 0) { // words[k] 不在子集 s 中
+                    continue;
+                }
+                for (int i = 0; i < words[k].length(); i++) {
+                    char c = words[k].charAt(i);
+                    wordCount[c - 'a']++;
+                }
+            }
+            boolean ok = true; // 判断子集 s 是否合法
+            int sum = 0; // 保存子集 s 的得分
+            for (int i = 0; i < 26; i++) {
+                sum += score[i] * wordCount[i];
+                ok = ok && (wordCount[i] <= count[i]);
+            }
+            if (ok) {
+                res = Math.max(res, sum);
+            }
+        }
+        return res;
+    }
 
+    static T1255 t1255 = new T1255();
+
+    public static void main(String[] args) {
+        System.out.println(t1255.maxScoreWords(new String[]{"dog", "cat", "dad", "good"},
+                new char[]{'a', 'a', 'c', 'd', 'd', 'd', 'g', 'o', 'o'},
+                new int[]{1, 0, 9, 5, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
     }
 }

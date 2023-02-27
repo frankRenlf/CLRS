b1716588362c1633250d3900640479a73ddbf1a1
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-27 09:46:09 +0800

    Create T1144.java

---
+++ clrs/src/leetcode/dailyWork/T1144.java
@@ -0,0 +1,19 @@
+package leetcode.dailyWork;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/27 9:45
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1144 {
+    public int movesToMakeZigzag(int[] nums) {
+
+    }
+}

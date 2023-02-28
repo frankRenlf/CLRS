e130f611b9c73e3a5bea01d551f352e1559f12b8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-28 09:57:18 +0800

    Create T2363.java

---
+++ clrs/src/leetcode/dailyWork/T2363.java
@@ -0,0 +1,21 @@
+package leetcode.dailyWork;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CLRS
+ * @Package : leetcode.dailyWork
+ * @createTime : 2023/2/28 9:56
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T2363 {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+
+    }
+}

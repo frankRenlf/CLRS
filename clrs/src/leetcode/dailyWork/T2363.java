18882be6483955c2de9c906633677734c73728d7
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-28 10:03:12 +0800

    Update T2363.java

--- clrs/src/leetcode/dailyWork/T2363.java
+++ clrs/src/leetcode/dailyWork/T2363.java
@@ -1,6 +1,6 @@
 package leetcode.dailyWork;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +16,25 @@ import java.util.List;
  */
 public class T2363 {
     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
-
+        List<List<Integer>> ret = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] i1 : items1) {
+            map.put(i1[0], i1[1]);
+        }
+        for (int[] i2 : items2) {
+            map.put(i2[0], map.getOrDefault(i2[0], 0) + i2[1]);
+        }
+        for (Map.Entry<Integer, Integer> el : map.entrySet()) {
+            List<Integer> list = new ArrayList<>();
+            list.add(el.getKey(), el.getValue());
+            ret.add(list);
+        }
+        ret.sort(new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return o1.get(1) - o2.get(1);
+            }
+        });
+        return ret;
     }
 }
